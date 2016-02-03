8073ff9d1daaee21c5b5b397a4032be3307551b5
Jacky Liang <coolboii@me.com>
2016-02-03 17:08:45 +0800

    Updated code

---
+++ DDNS53/ddns53.php
@@ -0,0 +1,102 @@
+<?php /* Amazon Route 53 DDNS Script
+------------------------------------------------------- */
+$aws_id  = $_GET['id'];
+$aws_secret = $_GET['secret'];
+$aws_zone = $_GET['zone'];
+
+$hostname = $_GET['hostname'];
+$type = $_GET['type'];
+$new_ip = $_GET['newip'];
+
+$old_ip = '';
+$record_type = '';
+$record_ttl = '';
+$route53 = '';
+$recordSet = '';
+
+$synology = ($_GET['synology'] == '1') ? true : false;
+$debug = ($_GET['debug'] == '1') ? true : false;
+
+/* Script Starts Below
+------------------------------------------------------- */
+require_once('r53.php');
+
+// Start a new instance
+$route53 = new Route53($aws_id, $aws_secret);
+
+// Define record set
+$recordSet = $route53->listResourceRecordSets('/hostedzone/' . $aws_zone);
+
+// Validate the authentication variables
+if(empty($aws_id) || empty($aws_secret) || empty($aws_zone) || empty($recordSet)) {
+	
+	if($synology == true) {
+		exit('(badauth)');
+	} else {
+		exit('Failed. Your AWS credentials and Zone ID information is incorrect or incomplete.');
+	}
+	
+}
+
+// Search for the old IP address
+for($i = 0; $i < count($recordSet['ResourceRecordSets']); $i++) {
+	
+	if($debug == true) {
+		echo '<b>Host:</b> ' . $recordSet['ResourceRecordSets'][$i]['Name'] . '<br />';
+		echo '<b>Host Value:</b> ' . $recordSet['ResourceRecordSets'][$i]['ResourceRecords'][0] . '<br />';
+		echo '<b>Type:</b> ' . $recordSet['ResourceRecordSets'][$i]['Type'] . '<br />';
+		echo '<b>TTL (sec):</b> ' . $recordSet['ResourceRecordSets'][$i]['TTL'] . '<br />';
+		echo '<br />';
+	}
+	
+	// Matches the hostname and the type
+	if($recordSet['ResourceRecordSets'][$i]['Name'] == $hostname . '.' && $recordSet['ResourceRecordSets'][$i]['Type'] == $type) {
+		
+		$old_ip			= $recordSet['ResourceRecordSets'][$i]['ResourceRecords'][0];
+		$record_type	= $recordSet['ResourceRecordSets'][$i]['Type'];
+		$record_ttl		= $recordSet['ResourceRecordSets'][$i]['TTL'];
+		
+	}
+	
+}
+
+// Process record changes
+if(empty($old_ip)) {
+	
+	if($synology == true) {
+		echo '(nohost)';
+	} else {
+		echo 'Failed. The hostname (<b>' . $hostname . '</b>) and type (<b>' . $type . '</b>) you specified yielded no results.';
+	}
+	
+} elseif(empty($new_ip)) {
+	
+	if($synology == true) {
+		echo '(badagent)';
+	} else {
+		echo 'Failed. A new IP address is required for this script to run.';
+	}
+	
+} elseif($old_ip == $new_ip) {
+	
+	if($synology == true) {
+		echo '(nochg)';
+	} else {
+		echo 'Failed. The old IP (<b>' . $old_ip . '</b>) is the same as the new IP (<b>' . $new_ip . '</b>).';
+	}
+	
+} else {
+	
+	$delete = $route53->prepareChange('DELETE', $hostname, $record_type, $record_ttl, $old_ip);
+	$create = $route53->prepareChange('CREATE', $hostname, $record_type, $record_ttl, $new_ip);
+	
+	$route53->changeResourceRecordSets('/hostedzone/' . $aws_zone, $delete);
+	$route53->changeResourceRecordSets('/hostedzone/' . $aws_zone, $create);
+	
+	if($synology == true) {
+		echo '(good)';
+	} else {
+		echo 'Success. The IP address for <b>' . $hostname . '</b> has been updated from <b>' . $old_ip . '</b> to <b>' . $new_ip . '</b>';
+	}
+	
+}
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-Usage: http://example.com/route53/ddns53.php?id=__USERNAME__&secret=__PASSWORD__&zone=YOURZONEID&hostname=__HOSTNAME__&myip=__MYIP__
+DDNS53
+=======
+
+DDNS53 is a URL-based PHP script designed to update your server's external hostname (e.g. server.example.com) using Amazon's Route 53 DNS service. It works automatically by calling the script via a URL (e.g. your web browser), authenticating using Amazon's IAM service, then changing the hostname value to the defined IP address. Additionally, it perfectly fits in with Synology NAS servers.
+
+__In other words, DDNS53 helps connect your web server's ever-changing IP address to a domain name.__
+
+To use this script, here are the steps you need to follow:
+
+1. Place in a locally accessible PHP server.
+2. Using the URL-method, call the script (usage below).
+3. Wait for the magic to begin!
+
+## Retrieve Variables
+These are the descriptions of the variables that you need to find before calling the script. Examples are located below (next to the description). Please follow the exact format unless you know what you're doing. __If you need help, feel free to open an issue on GitHub!__
+
+##### DDNS53 Authentication
+1. __id__ - Access Key ID (e.g. AKIRIQC23QKODRLJRV4A)
+2. __secret__ - Secret Access Key (e.g. Ikal5f2Hrk#XmiT5KAAnNj3OBd0DAoN9b1PFnFit)
+3. __zone__ - Hosted Zone ID (e.g. Z1W6QQYB7WYB0E)
+
+##### DDNS53 Configuration
+1. __hostname__ - Hostname (e.g. server.example.com)
+2. __type__ - Host Type (e.g. A, AAAA)
+3. __newip__ - New IP (e.g. 122.23.34.128)
+
+##### DDNS53 Optional Settings
+1. __synology__ - Enables Synology Error Messages (0 or 1, optional)
+2. __debug__ - Enabled Debug Mode (optional)
+
+## Get Amazon Access Keys
+Coming Soon!
+
+## Get Amazon Hosted Zone ID
+Coming Soon!
+
+## How to Use this Script
+There are plenty of ways on how to use this script, but to properly access this script, the authentication variables and the configuration variables must be set when you request this script. I have also included methods to enable Synology mode and debug mode for those who need them. Here are the examples:
+
+Usage: http://127.0.0.1/ddns53.php?id=__ACCESS KEY ID__&secret=__SECRET ACCESS KEY__&zone=__HOSTED ZONE ID__&hostname=__HOSTNAME__&type=__TYPE__&newip=__NEW IP ADDRESS__&debug=__DEBUG MODE__
+
+Synology: http://127.0.0.1/ddns53.php?id=__USERNAME__&secret=__PASSWORD__&zone=__YOURZONEID__&hostname=__HOSTNAME__&type=__type__&myip=__MYIP__
+
+## Credits
+Coming Soon!
